e21d478c635e8a2d3d5f60faff1f27bb20a61970
Marcin Eberst <marcin@v-zone.pl>
2023-01-08 17:37:53 +0100

    App Dio handling errors

--- lib/data/remote_data_sources/wather_remote_data_source.dart
+++ lib/data/remote_data_sources/wather_remote_data_source.dart
@@ -5,8 +5,18 @@ class WeatherRemoteDataSource {
   Future<Map<String, dynamic>?> getWeatherData({
     required String city,
   }) async {
-    final response = await Dio().get<Map<String, dynamic>>(
-        'http://api.weatherapi.com/v1/current.json?key=abc44451aeb24696b42194405230401&q=$city&aqi=no');
-    return response.data;
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
+          'http://api.weatherapi.com/v1/current.json?key=abc44451aeb24696b42194405230401&q=$city&aqi=no');
+      return response.data;
+    } on DioError catch (error) {
+      // if (error.response == null) {
+      //   throw Exception('Unknown error');
+      // } else {   <================================== To jest ten sam zapis co pod spodem
+      //   throw Exception(error.response!.data);
+      // }
+      throw Exception(
+          error.response?.data['error']['message'] ?? 'Błąd nieznany');
+    }
   }
 }
